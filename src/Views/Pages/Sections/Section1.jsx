c3e0fa485c08a6b23642fa3e6f186d9a359a6b2c
Yagnik Prajapati <prajapatiyagnik64@gmail.com>
2025-04-22 15:53:30 +0530

    updated

--- src/Views/Pages/Sections/Section1.jsx
+++ src/Views/Pages/Sections/Section1.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import PersonAddAltIcon from '@mui/icons-material/PersonAddAlt';
+import DownloadIcon from '@mui/icons-material/Download';
+
 
 const Section1 = () => {
-   
-    
+
+
     return (
         <>
             <section>
@@ -12,8 +15,31 @@ const Section1 = () => {
                             <div className="col-11  text-center">
                                 {/* <p className='fs-4'>Hello There! 👋</p> */}
                                 <p className=' text-uppercase text-highlight '>
-                                    Hi, I'm <span className='text-highlight-white'>Yagnik Prajapati</span> — a frontend developer 
+                                    Hi, I'm <span className='text-highlight-white'>Yagnik Prajapati</span> — a frontend developer
                                 </p>
+                            </div>
+                            <div className="col-12 my-3 text-center">
+                                <p className='text-success fw-bold'><span style={{ padding: "1px 10px" }} className='me-2 bg-success rounded-circle'></span>Available for Freelancing</p>
+                            </div>
+                            <div className="col-10  justify-content-center d-flex">
+                                {/* <!-- From Uiverse.io by mrhyddenn -->  */}
+                                <button className="button me-2">
+                                    <span className="button_lg">
+                                        <span className="button_sl"></span>
+                                        <span className="button_text">
+                                            Hire Me <PersonAddAltIcon style={{ fontSize: '18px', marginLeft: '6px' , marginBottom:"4px" }} />
+                                        </span>
+                                    </span>
+                                </button>
+
+                                <button className="button ms-2">
+                                    <span className="button_lg">
+                                        <span className="button_sl"></span>
+                                        <span className="button_text">
+                                            Download CV <DownloadIcon style={{ fontSize: '18px', marginLeft: '6px' }} />
+                                        </span>
+                                    </span>
+                                </button>
 
                             </div>
                         </div>
